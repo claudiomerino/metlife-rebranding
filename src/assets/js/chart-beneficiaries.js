609742756141541d73791fbe50ec448ee790a4ca
Jair <ejairn@gmail.com>
2017-03-03 10:19:29 -0500

    Archivos js independientes

--- src/assets/js/chart-beneficiaries.js
+++ src/assets/js/chart-beneficiaries.js
@@ -1,78 +1,80 @@
 
-	let ctx = document.getElementById("chartBeneficiaries");
+	if( document.getElementById("chartBeneficiaries") ) {
+		let ctx = document.getElementById("chartBeneficiaries");
 
-	let data = {
-		labels: [
-			"Esperanza del Carmen Risopatrón Goycochea",
-			"Maria Francisca Llanos Risopatrón",
-			"Felipe Martin Llanos Risopatrón",
-			"Maria Silva Lopez Llanos"
-		],
-		relation: [
-			"Conyugue",
-			"Hija",
-			"Hijo",
-			"Nieta"
-		],
-    datasets: [{
-			data: [
-				25,
-				25,
-				25,
-				25
+		let data = {
+			labels: [
+				"Esperanza del Carmen Risopatrón Goycochea",
+				"Maria Francisca Llanos Risopatrón",
+				"Felipe Martin Llanos Risopatrón",
+				"Maria Silva Lopez Llanos"
 			],
-			backgroundColor: [
-				"#0e4678",
-				"#e67f48",
-				"#947bba",
-				"#40967b"
-			]
-		}]
-	};
+			relation: [
+				"Conyugue",
+				"Hija",
+				"Hijo",
+				"Nieta"
+			],
+	    datasets: [{
+				data: [
+					25,
+					25,
+					25,
+					25
+				],
+				backgroundColor: [
+					"#0e4678",
+					"#e67f48",
+					"#947bba",
+					"#40967b"
+				]
+			}]
+		};
 
-	let options = {
-		animation: {
-			animateScale: true
-		},
-    legend: {
-    	display: false
-    }
-	}
+		let options = {
+			animation: {
+				animateScale: true
+			},
+	    legend: {
+	    	display: false
+	    }
+		}
 
-	let myDoughnutChart = new Chart(ctx, {
-	    type: 'doughnut',
-	    data: data,
-	    options: options
-	});
-
-	function customLabelsFn() {
-		let dataLabel = data.labels;
-		let dataRelation = data.relation;
-		let dataValue = data.datasets[0].data;
-		let dataBackgroundColor = data.datasets[0].backgroundColor;
-
-		let $chartTable = $( '#chartTable' );
-
-		$.each( dataLabel, function( key, dataLabelText ) {
-
-			let chartTemplate = `
-				<tr>
-		      <td bgcolor="${ dataBackgroundColor[key] }" width="10"></td>
-		      <td class="table-6" valign="middle">
-		      	<p class="table_label_title f-big">${ dataLabelText }</p>
-						<p class="table_desc f-small"><strong>Relación:</strong> ${ dataRelation[key] }</p>
-					</td>
-		      <td class="table-4" valign="middle">
-						<p class="table_desc f-big"><strong>${ dataValue[key] } %</strong></p>
-					</td>
-				</tr>
-			`;
-
-			$chartTable.append( chartTemplate );
-
+		let myDoughnutChart = new Chart(ctx, {
+		    type: 'doughnut',
+		    data: data,
+		    options: options
 		});
 
+		function customLabelsFn() {
+			let dataLabel = data.labels;
+			let dataRelation = data.relation;
+			let dataValue = data.datasets[0].data;
+			let dataBackgroundColor = data.datasets[0].backgroundColor;
 
+			let $chartTable = $( '#chartTable' );
+
+			$.each( dataLabel, function( key, dataLabelText ) {
+
+				let chartTemplate = `
+					<tr>
+			      <td bgcolor="${ dataBackgroundColor[key] }" width="10"></td>
+			      <td class="table-6" valign="middle">
+			      	<p class="table_label_title f-big">${ dataLabelText }</p>
+							<p class="table_desc f-small"><strong>Relación:</strong> ${ dataRelation[key] }</p>
+						</td>
+			      <td class="table-4" valign="middle">
+							<p class="table_desc f-big"><strong>${ dataValue[key] } %</strong></p>
+						</td>
+					</tr>
+				`;
+
+				$chartTable.append( chartTemplate );
+
+			});
+
+
+		}
+
+		customLabelsFn();
 	}
-
-	customLabelsFn();
--- src/assets/js/chart-investment.js
+++ src/assets/js/chart-investment.js
@@ -1,74 +1,76 @@
 
-	let ctx = document.getElementById("chartInvestment");
+	if( document.getElementById("chartInvestment") ) {
+		let ctx = document.getElementById("chartInvestment");
 
-	let dataInvestment = {
-		labels: [
-			"Fondo Mutuo Santander Global Emergente Serie Universal",
-			"Tasa Fija Garantizada en U.F.",
-			"Nombre del Fondo de Inversión con un nombre extremadamente largo"
-		],
-		relation: [
-			"Conyugue",
-			"Hija",
-			"Hijo"
-		],
-    datasets: [{
-			data: [
-				50,
-				35,
-				15
+		let dataInvestment = {
+			labels: [
+				"Fondo Mutuo Santander Global Emergente Serie Universal",
+				"Tasa Fija Garantizada en U.F.",
+				"Nombre del Fondo de Inversión con un nombre extremadamente largo"
 			],
-			backgroundColor: [
-				"#0e4678",
-				"#e67f48",
-				"#947bba"
-			]
-		}]
-	};
+			relation: [
+				"Conyugue",
+				"Hija",
+				"Hijo"
+			],
+	    datasets: [{
+				data: [
+					50,
+					35,
+					15
+				],
+				backgroundColor: [
+					"#0e4678",
+					"#e67f48",
+					"#947bba"
+				]
+			}]
+		};
 
-	let optionsInvestment = {
-		animation: {
-			animateScale: true
-		},
-    legend: {
-    	display: false
-    }
-	}
+		let optionsInvestment = {
+			animation: {
+				animateScale: true
+			},
+	    legend: {
+	    	display: false
+	    }
+		}
 
-	let myDoughnutInvestmentChart = new Chart(ctx, {
-	    type: 'doughnut',
-	    data: dataInvestment,
-	    options: optionsInvestment
-	});
-
-	function customInvestmentLabelsFn() {
-		let dataInvestmentLabel = dataInvestment.labels;
-		let dataInvestmentRelation = dataInvestment.relation;
-		let dataInvestmentValue = dataInvestment.datasets[0].data;
-		let dataInvestmentBackgroundColor = dataInvestment.datasets[0].backgroundColor;
-
-		let $chartInvestmentTable = $( '#chartInvestmentTable' );
-
-		$.each( dataInvestmentLabel, function( key, dataLabelText ) {
-
-			let chartInvestmentTemplate = `
-				<tr>
-		      <td bgcolor="${ dataInvestmentBackgroundColor[key] }" width="10"></td>
-		      <td class="table-6" valign="middle">
-		      	<p class="table_label_title f-big">${ dataLabelText }</p>
-						<p class="table_desc f-small"><strong>Relación:</strong> ${ dataInvestmentRelation[key] }</p>
-					</td>
-		      <td class="table-4" valign="middle">
-						<p class="table_desc f-big"><strong>${ dataInvestmentValue[key] } %</strong></p>
-					</td>
-				</tr>
-			`;
-
-			$chartInvestmentTable.append( chartInvestmentTemplate );
-
+		let myDoughnutInvestmentChart = new Chart(ctx, {
+		    type: 'doughnut',
+		    data: dataInvestment,
+		    options: optionsInvestment
 		});
 
+		function customInvestmentLabelsFn() {
+			let dataInvestmentLabel = dataInvestment.labels;
+			let dataInvestmentRelation = dataInvestment.relation;
+			let dataInvestmentValue = dataInvestment.datasets[0].data;
+			let dataInvestmentBackgroundColor = dataInvestment.datasets[0].backgroundColor;
 
+			let $chartInvestmentTable = $( '#chartInvestmentTable' );
+
+			$.each( dataInvestmentLabel, function( key, dataLabelText ) {
+
+				let chartInvestmentTemplate = `
+					<tr>
+			      <td bgcolor="${ dataInvestmentBackgroundColor[key] }" width="10"></td>
+			      <td class="table-6" valign="middle">
+			      	<p class="table_label_title f-big">${ dataLabelText }</p>
+							<p class="table_desc f-small"><strong>Relación:</strong> ${ dataInvestmentRelation[key] }</p>
+						</td>
+			      <td class="table-4" valign="middle">
+							<p class="table_desc f-big"><strong>${ dataInvestmentValue[key] } %</strong></p>
+						</td>
+					</tr>
+				`;
+
+				$chartInvestmentTable.append( chartInvestmentTemplate );
+
+			});
+
+
+		}
+
+		customInvestmentLabelsFn();
 	}
-
-	customInvestmentLabelsFn();
