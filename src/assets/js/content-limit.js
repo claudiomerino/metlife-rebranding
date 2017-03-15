8f70b2f849a34337481020d43d5b6942286a13ce
Jair <ejairn@gmail.com>
2017-03-15 15:01:52 -0500

    Update grid system

---
+++ src/assets/js/content-limit.js
@@ -0,0 +1,23 @@
+
+	let $contentLimit = $( '*[data-limit]' );
+	let $numberLimit = $contentLimit.val();
+
+	console.log( $numberLimit );
+
+	if( $contentLimit ) {
+
+		$.each( $contentLimit, ( k, v ) => {
+			let contentLimitLength = $( v ).text().length;
+			if( contentLimitLength > 35 ) {
+				let contentText = $( v ).text();
+				let $contentSubstring = $( v ).text().substring( 0, 35 );
+				let template = `<span data-tooltip aria-haspopup="true" class="has-tip top" data-disable-hover="false" tabindex="2" title="${contentText}">...</span>`;
+				$( v ).html( $contentSubstring + template );
+			}
+			else {
+				console.log( 'normal' );
+			}
+			console.log( contentLimitLength );
+		});
+
+	}
