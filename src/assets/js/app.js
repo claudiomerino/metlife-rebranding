5af2d3764bc6b71a50f7cdb47328334d75d30777
Jair <ejairn@gmail.com>
2017-06-02 20:53:40 -0500

    Arrows on select form it's work

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,16 @@
 
 	$(document).foundation();
+
+function tkObjectFit() {
+	console.log('test object fit');
+    $('img.objFit').each(function(){
+        var imgSrc = $(this).attr('src');
+        var fitType = 'cover';
+
+        if($(this).data('fit-type')) {
+            fitType = $(this).data('fit-type');
+        }
+
+        $(this).parent().css({ 'background' : 'transparent url("'+imgSrc+'") no-repeat center center/'+fitType, });
+        $(this).remove(); });
+}
