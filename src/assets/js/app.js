84600d86c8c938714318e439b105dc7bb2820673
sgarcia <sgarcia@svhen.com>
2017-06-05 07:22:29 -0500

    Change text select box

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,16 @@
 
 	$(document).foundation();
 
-function tkObjectFit() {
-	console.log('test object fit');
-    $('img.objFit').each(function(){
-        var imgSrc = $(this).attr('src');
-        var fitType = 'cover';
+    function tkObjectFit() {
+    	console.log('test object fit');
+        $('img.objFit').each(function(){
+            var imgSrc = $(this).attr('src');
+            var fitType = 'cover';
 
-        if($(this).data('fit-type')) {
-            fitType = $(this).data('fit-type');
-        }
+            if($(this).data('fit-type')) {
+                fitType = $(this).data('fit-type');
+            }
 
-        $(this).parent().css({ 'background' : 'transparent url("'+imgSrc+'") no-repeat center center/'+fitType, });
-        $(this).remove(); });
-}
+            $(this).parent().css({ 'background' : 'transparent url("'+imgSrc+'") no-repeat center center/'+fitType, });
+            $(this).remove(); });
+    }
