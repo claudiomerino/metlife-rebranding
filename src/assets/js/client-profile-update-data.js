87577c3478eaefd7c20d38e13926b16aac63c5e5
Jair <ejairn@gmail.com>
2017-06-16 15:27:22 -0500

    Validaciones en actualizar cliente

--- src/assets/js/client-profile-update-data.js
+++ src/assets/js/client-profile-update-data.js
@@ -4,21 +4,33 @@
 	const $clientProfileSaveUpdateData = $('#clientProfileSaveUpdateData')
 	const $clientProfileEdit = $('.clientProfileEdit')
 	const $clientProfileInitialData = $('.clientProfileInitialData')
+	const $ClientProfileToggleHide = $('.ClientProfileToggleHide')
 
 	$buttonUpdate.on('click', (e) => {
 		e.preventDefault()
 
-		$clientProfileEdit.removeClass('hide-xs')
-		$clientProfileInitialData.addClass('hide-xs')
-		$clientProfileCancelUpdateData.add($clientProfileSaveUpdateData).removeClass('hide-xs')
-		$buttonUpdate.addClass('hide-xs')
+		$clientProfileCancelUpdateData
+			.add($clientProfileSaveUpdateData)
+			.add($clientProfileEdit)
+			.removeClass('hide-xs')
+
+		$clientProfileInitialData
+			.add($buttonUpdate)
+			.add($ClientProfileToggleHide)
+			.addClass('hide-xs')
+
 	})
 
 	$clientProfileCancelUpdateData.add($clientProfileSaveUpdateData).on('click', (e) => {
 		e.preventDefault()
 
-		$clientProfileEdit.addClass('hide-xs')
-		$clientProfileInitialData.removeClass('hide-xs')
-		$clientProfileCancelUpdateData.add($clientProfileSaveUpdateData).addClass('hide-xs')
-		$buttonUpdate.removeClass('hide-xs')
+		$clientProfileCancelUpdateData
+			.add($clientProfileSaveUpdateData)
+			.add($clientProfileEdit)
+			.addClass('hide-xs')
+
+		$buttonUpdate
+			.add($clientProfileInitialData)
+			.add($ClientProfileToggleHide)
+			.removeClass('hide-xs')
 	})
