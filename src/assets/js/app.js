06ccc9bc0e93d2cee5ec4ab3e363c4f9593d1205
sgarcia <sgarcia@svhen.com>
2017-07-06 19:42:56 -0500

    Responsive tab-II Mutuaria

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,4 +3,40 @@
 
   $( document ).ready( function(e) {
       inputFilled();
+			var actualSize = Foundation.MediaQuery.current
+
+			console.log(actualSize, 'actualSize')
+
+		  let elem
+			let $testToggle = $('.testToggle');
+
+			function testToglefn() {
+				console.log(' se llama')
+
+				$testToggle.on( 'click', ( e ) => {
+					e.preventDefault();
+					$testToggle.toggleClass( 'test' );
+					console.log('First');
+				})
+			}
+
+			testToglefn();
+
+			if(actualSize === 'medium' || actualSize === 'small') {
+
+		  let element = $('#sampletabs')
+					new Foundation.ResponsiveAccordionTabs(element);
+					console.log('medium or small')
+			}
+
+		$(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+
+			console.log(event, newSize)
+			if( newSize === 'medium' || newSize === 'small' ) {
+				testToglefn();
+			}
+
+				console.log('se ejecuto segunda vez')
+		  // newSize is the name of the now-current breakpoint, oldSize is the previous breakpoint
+		});
   });
