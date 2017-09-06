8f99def86f53f03e43662d2d4cd30a0fc5ef0a8d
Jair <ejairn@gmail.com>
2017-09-06 17:21:24 -0500

    Adjust Seguros Individuales

--- src/assets/js/operations-mutuaria.js
+++ src/assets/js/operations-mutuaria.js
@@ -21,21 +21,48 @@
 		$SumInput.each( (index, data) => {
 			const $data = $(data)
 
+			console.log($data.is(':checked'), 'checked')
+
 			if( $data.is(':checked') ) {
 				sumTotal += parseFloat( $( data ).data('value-sum-uf') )
 
-				console.log($data, '$data')
 				$SumTotalUF.text( numeral(sumTotal).format( '0,0.00' ) )
 				$SumTotalPeso.text( numeral( sumTotal * UFVALUE ).format('0,0') )
 
 				setTimeout( () => {
 					if($data.attr('disabled') == 'disabled') {
 						sumTotal -= parseFloat( $( data ).data('value-sum-uf') )
-						$SumTotalUF.text( numeral(sumTotal).format( '0,0.00' ) )
-						$SumTotalPeso.text( numeral( sumTotal * UFVALUE ).format('0,0') )
 						console.log($( data ).data('value-sum-uf'), 'sumTotal bounty')
 					}
+						if ($(".BountyRowWrap .SumInput:checkbox:checked").length > 0)
+						{
+							console.log('hay checkado uno')
+						    // any one is checked
+						}
+						else {
+							sumTotal = 0
+							console.log('NO hay checkado uno')
+
+						}
+
+						$SumTotalUF.text( numeral(sumTotal).format( '0,0.00' ) )
+						$SumTotalPeso.text( numeral( sumTotal * UFVALUE ).format('0,0') )
 				}, 10)
+			}
+			else {
+				if ($(".BountyRowWrap .SumInput:checkbox:checked").length > 0)
+				{
+					console.log('hay checkado uno')
+				    // any one is checked
+				}
+				else {
+					sumTotal = 0
+					console.log('NO hay checkado uno')
+					$SumTotalUF.text( numeral(sumTotal).format( '0,0.00' ) )
+					$SumTotalPeso.text( numeral( sumTotal * UFVALUE ).format('0,0') )
+
+				}
+
 			}
 
 		})
