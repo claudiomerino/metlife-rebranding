4d0618ebf91a07e010dfbb356fb7df78d9dc063c
Jair <ejairn@gmail.com>
2017-09-24 15:18:11 -0500

    Pagos en línea validation

--- src/assets/js/instalar-mandato-pat.js
+++ src/assets/js/instalar-mandato-pat.js
@@ -7,7 +7,24 @@
 
 	  const options = {
 	    onChange : function( val ) {
-				if( val == 'allStatePoliza' ) {
+
+				if( val == 'allStatePoliza' || $('select.DataSelectCard.cs-select').val() == null ) {
+					$DataSelectPolizaSubmit.addClass('button-disabled')
+				}
+				else {
+					$DataSelectPolizaSubmit.removeClass('button-disabled')
+				}
+	    }
+	  }
+	  new SelectFx(el, options);
+	});
+
+	[].slice.call( document.querySelectorAll( 'select.DataSelectCard.cs-select' ) ).forEach( (el) => {
+
+	  const options = {
+	    onChange : function( val ) {
+
+				if( val == 'allStatePoliza' || $('select.DataSelectPoliza.cs-select').val() == null ) {
 					$DataSelectPolizaSubmit.addClass('button-disabled')
 				}
 				else {
