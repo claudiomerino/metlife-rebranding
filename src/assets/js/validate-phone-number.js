b9735c513aca3ad9d7d41ada507ca625f2563b66
Jair <ejairn@gmail.com>
2017-09-25 18:21:51 -0500

    Regex phone pattern

--- src/assets/js/validate-phone-number.js
+++ src/assets/js/validate-phone-number.js
@@ -1,6 +1,4 @@
 
 	$('.ValidatePhoneNumber').on('input', function (event) {
-		console.log(event, 'ValidatePhoneNumber')
-		this.value = this.value.replace(/\+?([^0-9])?[-. ]?([^0-9])[-. ]?([^0-9])$/, '');
-		//this.value = this.value.replace(/[^0-9]/g, '');
+		this.value = this.value.replace(/[\+]?[(]?[^0-9][)]?[-\s\.]?[^0-9][-\s\.]?[^0-9]$/im, '');
 	});
