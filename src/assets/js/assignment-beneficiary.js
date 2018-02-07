76c0dbdd29f3df1b3f5b479706ce94f2a827f783
svhen <sgarcia@svhen.com>
2018-02-07 12:42:23 -0500

    Solicitud Edicion de Beneficiarios: Funcionalidad porcentaje de Asignación

--- src/assets/js/assignment-beneficiary.js
+++ src/assets/js/assignment-beneficiary.js
@@ -2,9 +2,9 @@
 
 	/*=====  Put all values for the input Assignment  ======*/
 	function valuesAssignmentFn() {
-		console.log('delete executed')
 		let $assignmentFieldValue = $( '.assignmentFieldValue' )
 		let $assignmentTotal = $( '.assignmentTotal' )
+		let $editBeneficiaryButton = $( '.editBeneficiaryButton' )
 
 		let $assignmentEmptyArray = []
 		let sumAssignment = 0
@@ -12,11 +12,12 @@
 		$assignmentFieldValue.map( (key, el) => {
 			let valueInputAssignment
 
-			if( $(el).val() ) {
+			if( $(el).val() && $(el).val() > 0 ) {
 				valueInputAssignment = $(el).val()
 				$(el).removeClass('is-invalid-input')
 				sumAssignment += parseInt( valueInputAssignment )
 			}
+
 			else {
 				$assignmentEmptyArray.push( 'error' )
 				$(el).addClass('is-invalid-input')
@@ -30,21 +31,23 @@
 		/*=====  All fields assignment not has a value with error. If is -1 not find any error  ======*/
 		if( $assignmentEmpty == -1 ) {
 			$assignmentTotalText.removeClass('red')
+			$editBeneficiaryButton.removeClass('button-disabled')
 
 			/*=====  If Sum total is equal to 100  ======*/
 			if( sumAssignment == 100 ) {
 				$assignmentTotalText.removeClass('red')
+				$editBeneficiaryButton.removeClass('button-disabled')
 			}
 			else {
 				$assignmentTotalText.addClass('red')
+				$editBeneficiaryButton.addClass('button-disabled')
 			}
 
 		}
 		else {
 			$assignmentTotalText.addClass('red')
+			$editBeneficiaryButton.addClass('button-disabled')
 		}
-
-		console.log( sumAssignment, 'sumAssignment' )
 
 		$assignmentTotal.html( sumAssignment )
 
